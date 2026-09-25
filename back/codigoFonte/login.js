6c69cf476454f6dd3d1e951129604c812f3ebc01
Davi Perrelli <davisouzape15@gmail.com>
2026-09-25 12:05:03 -0300

    feat(login): alterna ícone do botão de mostrar senha

--- back/codigoFonte/login.js
+++ back/codigoFonte/login.js
@@ -1,11 +1,15 @@
 const VerSenha = document.getElementById('password');
+const icon = document.getElementById('icon')
+
 
 //Botão de mostrar senha login
 document.querySelector('#showpassword').addEventListener('click', function() {
 if (VerSenha.type === 'password') {
 VerSenha.type = 'text';
+        icon.classList.replace('fa-eye', 'fa-eye-slash');
 } 
 else {
         VerSenha.type = 'password';
+        icon.classList.replace('fa-eye-slash', 'fa-eye');
 }
 });
