3fa68c8040a28013d6fa64e5984b934a6348dd2a
Davi Perrelli <davisouzape15@gmail.com>
2026-09-25 12:26:10 -0300

    feat(cadastro): alterna ícone do botão de mostrar senha

--- back/codigoFonte/cad.js
+++ back/codigoFonte/cad.js
@@ -1,23 +1,29 @@
 const VerSenha = document.getElementById('password');
 const VerSenhaConfi = document.getElementById('confirm-password');
+const icon = document.getElementById('icon')
+const iconc = document.getElementById('icon-confi')
 
 
 //Botão de mostrar senha
 document.querySelector('#showpassword').addEventListener('click', function() {
 if (VerSenha.type === 'password') {
-VerSenha.type = 'text';
+        VerSenha.type = 'text';
+        icon.classList.replace('fa-eye', 'fa-eye-slash');
 } 
 else {
         VerSenha.type = 'password';
+        icon.classList.replace('fa-eye-slash', 'fa-eye');
 }
 });
 
 //Botão de mostrar senha confirmada
 document.querySelector('#showconfirm-password').addEventListener('click', function() {
 if (VerSenhaConfi.type === 'password') {
-VerSenhaConfi.type = 'text';
+        VerSenhaConfi.type = 'text';
+        iconc.classList.replace('fa-eye', 'fa-eye-slash');
 } 
 else {
         VerSenhaConfi.type = 'password';
+        iconc.classList.replace('fa-eye-slash', 'fa-eye');
 }
 });
